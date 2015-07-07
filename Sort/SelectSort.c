686980a74247ca13416294eeb7440fe60044343f
TaiHeng W <taihengw@gmail.com>
2015-07-07 15:23:08 +0800

    Overview SelectSort

--- Sort/SelectSort.c
+++ Sort/SelectSort.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
+/* Don't you see it? I don't use a pointer */
 void SelectSort(int r[], int n)
 {
     int i, j, t, m;
     for (i = 0; i < n-1; i++) {
         t = i;
-        for(j = i+1; j < n; j++) {
+        for(j = i+1; j < n; j++) { /* Find min in the n-i elements */
             if(r[j] < r[t]) {
                 t = j;
             }
         }
-        if(t != i) {
+        if(t != i) {            /* swap min to proper position */
             m = r[i];
             r[i] = r[t];
             r[t] = m;
